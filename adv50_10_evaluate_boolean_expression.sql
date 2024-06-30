a396e29d7466ae3d60372a6633400d273f479181
jeganpillai <jegan.velappan@gmail.com>
2024-06-30 00:35:06 -0700

    Create adv50_10_evaluate_boolean_expression.sql
    initial version

---
+++ adv50_10_evaluate_boolean_expression.sql
@@ -0,0 +1,65 @@
+-- Question: Evaluate Boolean Expression
+
+-- English Video: 
+-- Tamil Video: 
+
+Create Table If Not Exists Variables (name varchar(3), value int);
+Truncate table Variables;
+insert into Variables (name, value) values 
+ ('x', 66)
+,('y', 77);
+
+Create Table If Not Exists Expressions (
+  left_operand varchar(3), 
+  operator ENUM('>', '<', '='), 
+  right_operand varchar(3));
+Truncate table Expressions;
+insert into Expressions (left_operand, operator, right_operand) values 
+ ('x', '>', 'y')
+,('x', '<', 'y')
+,('x', '=', 'y')
+,('y', '>', 'x')
+,('y', '<', 'x')
+,('x', '=', 'x');
+
+/*
++--------------+----------+---------------+-------+
+| left_operand | operator | right_operand | value |
++--------------+----------+---------------+-------+
+| x            | >        | y             | false |
+| x            | <        | y             | true  |
+| x            | =        | y             | false |
+| y            | >        | x             | true  |
+| y            | <        | x             | false |
+| x            | =        | x             | true  |
++--------------+----------+---------------+-------+
+*/
+
+-- Approach 1: Manage in single CASE statement 
+select e.left_operand,
+       e.operator,
+       e.right_operand,
+       case when e.operator = '>' and vl.value > vr.value then 'true'
+            when e.operator = '<' and vl.value < vr.value then 'true'
+            when e.operator = '=' and vl.value = vr.value then 'true'
+            else 'false' end value 
+       from Expressions e 
+ inner join Variables vl
+         on vl.name = e.left_operand 
+ inner join Variables vr
+         on vr.name = e.right_operand;
+
+-- Approach 2: Clusterted CASE statement 
+select e.left_operand,
+       e.operator,
+       e.right_operand,
+       case when 
+       case when e.operator = '>' then vl.value > vr.value 
+            when e.operator = '<' then vl.value < vr.value
+            when e.operator = '=' then vl.value = vr.value
+       end = 1 then 'true' else 'false' end as value 
+       from Expressions e 
+ inner join Variables vl
+         on vl.name = e.left_operand 
+ inner join Variables vr
+         on vr.name = e.right_operand;
