6a7d1991854cb484a575fadbba5c5be15118c9e7
jeganpillai <jegan.velappan@gmail.com>
2024-07-09 02:18:47 -0700

    Create adv50_15_customers_with_maximum_order.sql
    initial version

---
+++ adv50_15_customers_with_maximum_order.sql
@@ -0,0 +1,70 @@
+-- Question: Find the Customers with Maximum Order
+
+-- English Video: 
+-- Tamil Video: 
+
+Create table orders (order_number int, 
+                     customer_number int);
+                     
+Truncate table orders;
+insert into orders (order_number, customer_number) values 
+( 5,  1),
+( 9,  2),
+(12,  3),
+(14,  3),
+( 7,  4),
+(10,  4),
+( 3,  5),
+( 6,  5),
+(13,  5),
+( 8,  6),
+(11, 16),
+(15, 16);
+
+/*
++-----------------+
+| customer_number |
++-----------------+
+| 5               |
++-----------------+
+*/
+
+-- Approach 1: Basic SQL with subquery
+with all_data as (
+select customer_number, count(*) as cnt 
+       from Orders o 
+   group by 1)
+select customer_number 
+       from all_data 
+      where cnt = (select max(cnt) from all_data);
+
+-- Approach 2: Using Windows Function 
+with all_data as (
+select customer_number,
+       count(*) over(partition by customer_number) as cnt 
+       from Orders o )
+select distinct customer_number 
+       from all_data 
+      where cnt = (select max(cnt) from all_data);
+
+-- Approach 3: Using Windows Function with Distinct option 
+with all_data as (
+select distinct customer_number,
+       count(*) over(partition by customer_number) as cnt
+       from orders )
+select customer_number 
+       from all_data 
+      where cnt = (select max(cnt) from all_data);
+
+-- Approach 4: Automate using Windows Function 
+with all_data as (
+select distinct customer_number,
+       count(*) over(partition by customer_number) as cnt
+       from orders )
+,top_customers as (
+select customer_number,
+       row_number() over(order by cnt desc) as rnk 
+       from all_data )
+select customer_number 
+       from top_customers
+      where rnk = 1;
